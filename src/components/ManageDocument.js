cbdeaeb9495bffd0d81483c82ac09774b5bef481
pradeepkumar-es <createthefuture121@gmail.com>
2024-10-09 17:13:07 +0530

    new update to main panel

--- src/components/ManageDocument.js
+++ src/components/ManageDocument.js
@@ -1,175 +1,233 @@
-import React, { useState,useEffect } from 'react'
-import { storage,database } from '../firebase'
-import { MdCloudUpload } from "@react-icons/all-files/md/MdCloudUpload";
-import Logout from './Logout'
-import { Link } from 'react-router-dom'
-import {ref, 
-         uploadBytesResumable,
-         getDownloadURL} from 'firebase/storage'
-import { collection,
-          addDoc, 
-         doc,  //for update doc
-         updateDoc, //for update doc
-         deleteDoc,
-         onSnapshot, //for get Data //for real time update
-         query, where
-      } from 'firebase/firestore';
-import { v4 } from 'uuid';   //gor generating unique id for different uploads to rename it
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import MyProfile from './MyProfile'
+import React, { useState, useEffect } from "react";
+import { storage, database } from "../firebase";
+import { MdCloudUpload } from "react-icons/md";
+import { MdOutlineEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import Logout from "./Logout";
+import { Link } from "react-router-dom";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {
+  collection,
+  addDoc,
+  doc, //for update doc
+  updateDoc, //for update doc
+  deleteDoc,
+  onSnapshot, //for get Data //for real time update
+  query,
+  where,
+} from "firebase/firestore";
+import { v4 } from "uuid"; //gor generating unique id for different uploads to rename it
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import MyProfile from "./MyProfile";
 
-function ManageDocument() { //functional component to manage document
-  const [array, setArray] = useState([])
-  const [uploadProgress, setUploadprogress] = useState('')
-  const [imgUrl, setImgUrl] =useState('')
-  const [docName, setDocName]=useState('')
-  const [user,setUser] = useState(null)
+function ManageDocument() {
+  //functional component to manage document
+  const [array, setArray] = useState([]);
+  const [uploadProgress, setUploadprogress] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [docName, setDocName] = useState("");
+  const [user, setUser] = useState(null);
 
-const auth= getAuth(); //firebase
-  useEffect(()=>{
-    onAuthStateChanged(auth, (user)=>{
-    if(user){
-        setUser(user)
-        getDetails(user)
-    }else {
-        setUser(null)
-    }
-    })
-   },[auth])
-  
-  const addDocument=async(userdata)=>{
-    const collectionRef= collection(database, 'users')
+  const auth = getAuth(); //firebase
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        getDetails(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [auth]);
+
+  const addDocument = async (userdata) => {
+    const collectionRef = collection(database, "users");
     await addDoc(collectionRef, {
       documentName: docName,
       imageUrl: imgUrl,
-      userId : userdata.uid
-  })
-  .then(() => {
-    alert("Document Added Successfully")
-    getDetails()
-  })
-  .catch((error) => {
-    // Handle error
-    alert(error.message)
-  });
-  }
-  const handleAdd=()=>{
+      userId: userdata.uid,
+    })
+      .then(() => {
+        alert("Document Added Successfully");
+        getDetails();
+      })
+      .catch((error) => {
+        // Handle error
+        alert(error.message);
+      });
+  };
+  const handleAdd = () => {
     addDocument(user);
-  }
-  const handleUpload=(event)=>{
-        const storageRef = ref(storage, `images/${v4()}`)
-        const uploadTask = uploadBytesResumable(storageRef, event.target.files[0]);
-            // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on('state_changed',
-        (snapshot) => {
+  };
+  const handleUpload = (event) => {
+    const storageRef = ref(storage, `images/${v4()}`);
+    const uploadTask = uploadBytesResumable(storageRef, event.target.files[0]);
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setUploadprogress('Upload is ' + progress + '% done');
-            }, 
-        (err) => {
-        alert(err.message)
-        }, 
-        () => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadprogress("Upload is " + progress + "% done");
+      },
+      (err) => {
+        alert(err.message);
+      },
+      () => {
         // Upload completed successfully, now we can get the download URL
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setImgUrl(downloadURL);
+          setImgUrl(downloadURL);
         });
-        }
-        );
-  }
+      }
+    );
+  };
 
-  //getting data to display on browser 
-  
-  const getDetails = async(userdata)=>{
-    const collectionRef= collection(database, 'users')
-    const ageQuery =  query(collectionRef, where("userId", "==" , `${userdata.uid}`))
+  //getting data to display on browser
 
-                //for real time update
-             onSnapshot(ageQuery, (response)=>{  //for selected response
-                        setArray(
-                                    response.docs.map((item)=>{
-                                        return {...item.data(), id: item.id};
-                                    }))
-                    })
-}
-useEffect(()=>{
-  getDetails(user)
-},[user])
+  const getDetails = async (userdata) => {
+    if (!userdata || !userdata.uid) {
+      console.error("userdata is null or uid is missing");
+      return;
+    }
+    const collectionRef = collection(database, "users");
+    const ageQuery = query(
+      collectionRef,
+      where("userId", "==", `${userdata.uid}`)
+    );
 
-        const updateData=(id)=>{
-          const docToUpdate = doc(database, 'users', id ) //in the last parameter it is  id
-          updateDoc(docToUpdate,{
-            documentName: docName,
-            // imageUrl: imgUrl
-          })
-          .then(()=>{
-              alert("Data Updated successfully")
-              getDetails()
-          })
-          .then((error)=>{
-              alert(error.message);
-          })
-        }
-        const deleteData=(id)=>{
-          const docToDelete = doc(database, 'users', id)
-          deleteDoc(docToDelete)
-          .then(()=>{
-              alert("Data deleted successfully")
-              getDetails()
-          })
-          .catch((err)=>{
-              alert(err.message)
-          })
-        }
+    //for real time update
+    onSnapshot(ageQuery, (response) => {
+      //for selected response
+      setArray(
+        response.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+    });
+  };
+  useEffect(() => {
+    if(user){
+      getDetails(user);
+    }
+  }, [user]);
+
+  const updateData = (id) => {
+    const docToUpdate = doc(database, "users", id); //in the last parameter it is  id
+    updateDoc(docToUpdate, {
+      documentName: docName,
+      // imageUrl: imgUrl
+    })
+      .then(() => {
+        alert("Data Updated successfully");
+        getDetails();
+      })
+      .then((error) => {
+        alert(error.message);
+      });
+  };
+  const deleteData = (id) => {
+    const docToDelete = doc(database, "users", id);
+    deleteDoc(docToDelete)
+      .then(() => {
+        alert("Data deleted successfully");
+        getDetails();
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
-    <div className='documentDashboard'>
-      <div className='sidebar'>
-        <div className='nav-tab'>
-          Hello
-        </div>
+    <div className="documentDashboard">
+      <div className="sidebar">
+        <div className="nav-tab">Hello</div>
         <Logout />
       </div>
-      <div className='mainPanel'>
-      <p>My Documents</p>
-      <p>{uploadProgress}</p>
-      <input type="text" name='documentName' placeholder='Document Name' onChange={(event)=>setDocName(event.target.value)} />
-      <form className='inputForm' action="" onClick={()=>{document.getElementById("uploadInput").click()}}> {/* click() method simulates a mouse click on an element.*/}
-      <input id='uploadInput' type="file" name='document' onChange={(event)=>handleUpload(event)}  />
-      <div>
-      <MdCloudUpload className='uploadIcons'/>
-      </div>
-      
-      </form>
-      <button onClick={handleAdd}>Add Document</button>
-      {/* <button onClick={addDocument}>Add Document</button> */}
-       {/* <p>{user.uid}</p>
-      <p>{user.email}</p>  */}
-      {/* displaying data if user is present */}
-      <h3>Files</h3>
-      <div className='Alldocument'>
-      {(array.length>0)? (
-        array.map((content)=>{
-          return(
-          <div className='document' >
-            <div >
-            <img className='docImage' src={content.imageUrl}  alt='document' /><br />
-            </div>
-            <h4>{content.documentName}</h4>
-            <input type="text" name='documentName' placeholder='Document Name' onChange={(event)=>setDocName(event.target.value)} />
-            <button onClick={()=>updateData(content.id)}>update Document Name</button>
-            <button onClick={()=>deleteData(content.id)}>Delete Document</button>
-            {/* <hr /> */}
+      <div className="mainPanel">
+        <h1 className="mainPanelHeading">My Documents</h1>
+        <h3 className="uploadPanelHeading">Upload Your File</h3>
+        <form
+          className="inputForm"
+          action=""
+          onClick={() => {
+            document.getElementById("uploadInput").click();
+          }}
+        >
+          {" "}
+          {/* click() method simulates a mouse click on an element.*/}
+          <input
+            id="uploadInput"
+            type="file"
+            name="document"
+            onChange={(event) => handleUpload(event)}
+          />
+          <div>
+            <MdCloudUpload className="uploadIcons" />
           </div>
-          )
-        })) 
-        : (
-          <p>No Document Found! Add Your Document securely to get accessible everywhere online</p>
-        )
-      }
-      </div>
+          <div>
+            <p className="progressText">{uploadProgress}</p>
+          </div>
+        </form>
+        <div className="AddDocAction">
+          <input
+            className="docInput"
+            type="text"
+            name="documentName"
+            placeholder="Document Name"
+            onChange={(event) => setDocName(event.target.value)}
+          />
+          <button onClick={handleAdd}>Add Document</button>
+        </div>
+        {/* <button onClick={addDocument}>Add Document</button> */}
+        {/* <p>{user.uid}</p>
+      <p>{user.email}</p>  */}
+        {/* displaying data if user is present */}
+        <h3 className="filePanel-heading">My Uploaded Files</h3>
+        <div className="Alldocument">
+          {array.length > 0 ? (
+            array.map((content) => {
+              return (
+                <div className="document">
+                  <div>
+                    <img
+                      className="docImage"
+                      src={content.imageUrl}
+                      alt="document"
+                    />
+                    <MdDelete className="deleteDoc" title="Delete" onClick={()=>deleteData(content.id)}  size={15}/>
+                    <br />
+                  </div>
+                  <div className="uploadedDoc">
+                    <h4 className="uploadedDocName">
+                      {content.documentName}
+                      <MdOutlineEdit style={{ color: "red" }} size={18} />
+                    </h4>
+                  </div>
+                  <div className="updateDoc">
+                  <input
+                    type="text"
+                    name="documentName"
+                    placeholder="Document Name"
+                    onChange={(event) => setDocName(event.target.value)}
+                  />
+                  <button onClick={() => updateData(content.id)}>
+                    Update
+                  </button>
+                  </div>
+                  {/* <hr /> */}
+                </div>
+              );
+            })
+          ) : (
+            <p>
+              No Document Found! Add Your Document securely to get accessible
+              everywhere online
+            </p>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ManageDocument
+export default ManageDocument;
